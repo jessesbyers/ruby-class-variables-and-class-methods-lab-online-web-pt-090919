ff85a09de33188d39fb394503648478e283e8fc9
Jesse Byers <jesse.s.byers@gmail.com>
2019-09-23 14:58:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,9 +30,9 @@ end
 def self.genre_count
   genre_hash = {}
   @@genres.each do |word|
-      genre_hash[word] = nil
-      genre_hash.each do |key, value|
+      genre_hash[word] = 1
     if genre_hash.has_key?(word)
+      genre_hash.each do |key, value|
       genre_hash[word] = value + 1
     else
       genre_hash[word] = 1
